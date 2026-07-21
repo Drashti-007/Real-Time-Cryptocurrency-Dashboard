396e64324b5ed46c6f1d338f2ef9f931697c7983
Drashti Gadhvi <gadhvidrashti007@gmail.com>
2026-07-21 21:40:46 +0530

    updated to choose currency

--- app.py
+++ app.py
@@ -11,12 +11,18 @@ st.set_page_config(
     layout="wide"
 )
 
+st.sidebar.title("⚙️ Settings")
+currency = st.sidebar.selectbox(
+    "Select Currency",
+    ["usd", "inr", "eur", "gbp"]
+)
+
 st_autorefresh(interval=30000, key="crypto_refresh")
 
 st.title("📈 Live Cryptocurrency Dashboard")
 st.write("Real-time cryptocurrency prices using the CoinGecko API")
 
-url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana,dogecoin&vs_currencies=usd"
+url = f"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,solana,dogecoin&vs_currencies={currency}"
 
 response = requests.get(url)
 
@@ -35,41 +41,43 @@ crypto_prices = []
 
 for crypto in data:
     crypto_names.append(crypto.capitalize())
-    crypto_prices.append(data[crypto]['usd'])
+    crypto_prices.append(data[crypto][currency])
 
 df = pd.DataFrame({
     "Cryptocurrency": crypto_names,
-    "Price (USD)": crypto_prices,
+    f"Price ({currency.upper()})": crypto_prices,
     "Timestamp": [current_time] * len(crypto_names)
 })
 
-df["Price (USD)"] = df["Price (USD)"].round(2)
+currency_symbols = {
+    "usd": "$",
+    "inr": "₹",
+    "eur": "€",
+    "gbp": "£"
+}
 
-df = df.sort_values(
-    by="Price (USD)",
-    ascending=False
-)
+symbol = currency_symbols[currency]
 
 col1, col2, col3, col4 = st.columns(4)
 
 with col1:
-    st.metric("₿ Bitcoin", f"${data['bitcoin']['usd']:,}")
+    st.metric("₿ Bitcoin", f"{symbol}{data['bitcoin'][currency]:,}")
 
 with col2:
-    st.metric("Ξ Ethereum", f"${data['ethereum']['usd']:,}")
+    st.metric("Ξ Ethereum", f"{symbol}{data['ethereum'][currency]:,}")
 
 with col3:
-    st.metric("◎ Solana", f"${data['solana']['usd']:,}")
+    st.metric("◎ Solana", f"{symbol}{data['solana'][currency]:,}")
 
 with col4:
-    st.metric("🐶 Dogecoin", f"${data['dogecoin']['usd']}")
+    st.metric("🐶 Dogecoin", f"{symbol}{data['dogecoin'][currency]}")
 
 st.write(f"🕒 Last Update: {current_time}")
 
 st.subheader("Current Prices")
 st.dataframe(
     df.style.format({
-        "Price (USD)": "${:,.2f}"
+        "Price ({currency})": "${:,.2f}"
     }),
     use_container_width=True
 )
